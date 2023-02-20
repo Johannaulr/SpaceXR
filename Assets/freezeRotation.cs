8398893aff463dd7715238a4bfbb5b681a4432b7
Johanna <113611870+Johannaulr@users.noreply.github.com>
2023-02-20 12:52:23 +0100

    Unity Files

---
+++ Assets/freezeRotation.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class freezeRotation : MonoBehaviour
+{
+    private Quaternion lastParentRotation;
+    void Start()
+    {
+        lastParentRotation = transform.parent.localRotation;
+    }
+
+        void Update()
+    {
+        transform.localRotation = Quaternion.Inverse(transform.parent.localRotation) *
+            lastParentRotation *
+            transform.localRotation;
+
+
+        lastParentRotation = transform.parent.localRotation;
+    }
+       
+}
