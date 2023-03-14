6355c4361272d5a819a6bd4751d38561588cf14b
Tanmay Pradhan <tanmaypradhan1990@gmail.com>
2023-03-14 23:04:45 +0100

    Position Update/ Reset Position Script

---
+++ Assets/Scripts/resetPoint.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resetPoint : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform originPoint;
+    [SerializeField]
+    GameObject player;
+    [SerializeField] Camera playerHead;
+    
+    [ContextMenu("Reset Position")]
+    public void ResetPosition()
+    {
+        var rotationAngleY = originPoint.rotation.eulerAngles.y - playerHead.transform.eulerAngles.y;
+        player.transform.Rotate(0, rotationAngleY, 0);
+
+        var distanceDiff = originPoint.position - playerHead.transform.position;
+        player.transform.position += distanceDiff;
+    }
+}
